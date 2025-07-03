a0f4e324e64dd608ecfc9b811737ed9fa4192a7b
sarank1634 <saravanakumar1634@gmail.com>
2025-07-03 15:20:19 +0530

    3-july pagination final update

--- todo_list/src/Create.jsx
+++ todo_list/src/Create.jsx
@@ -18,35 +18,3 @@ function Create() {
 }
 
 export default Create;
-
-
-// import { useState } from "react";
-// import axios from "axios";
-
-// function Create({ onAdd }) {
-//   const [task, setTask] = useState("");
-
-//   const handleAdd = () => {
-//     if (!task.trim()) return;
-//     axios.post("http://localhost:3000/add", { task })
-//       .then(() => {
-//         setTask("");
-//         Add(); // Refresh todo list
-//       })
-//       .catch(err => console.error("Add error:", err));
-//   };
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         value={task}
-//         onChange={(e) => setTask(e.target.value)}
-//         placeholder="Enter todo"
-//       />
-//       <button onClick={handleAdd}>Add</button>
-//     </div>
-//   );
-// }
-
-// export default Create;
